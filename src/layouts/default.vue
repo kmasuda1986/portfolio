c722a78abe8dc65d2c3132675f171abbbaca5ea9
kmasuda1986 <kentarou_masuda@hacklab-tech.io>
2022-04-26 09:35:37 +0900

    first commit

---
+++ src/layouts/default.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer
+      v-model="data.drawer"
+      :mini-variant="data.miniVariant"
+      :clipped="data.clipped"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in data.items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar :clipped-left="data.clipped" fixed app>
+      <v-app-bar-nav-icon @click.stop="data.drawer = !data.drawer" />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-navigation-drawer v-model="data.rightDrawer" :right="data.right" temporary fixed>
+      <v-list>
+        <v-list-item @click.native="right = !right">
+          <v-list-item-action>
+            <v-icon light> mdi-repeat </v-icon>
+          </v-list-item-action>
+          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-footer :absolute="!data.fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'DefaultLayout',
+
+  setup() {
+    const data = ref({
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/',
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire',
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js',
+    })
+
+    return {
+      data
+    }
+  },
+})
+</script>
---
+++ src/pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col
+      class="text-center"
+      cols="12"
+    >
+      <h1>Let's <span class="light-blue--text">View</span> Portfolio</h1>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'IndexPage',
+
+  setup() {
+    
+  },
+})
+</script>
+
+<style scoped>
+
+</style>
