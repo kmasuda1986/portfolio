a4b974556ba40bf977e1040e7dd42aa18fe278ad
増田健太郎 <kentarou_masuda@hacklab-tech.io>
2022-05-02 17:27:34 +0900

    fix: 修正

---
+++ src/components/atoms/HashTag.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-chip
+    label
+    small
+    v-text="`# ${props.text}`"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'AtomsHashTag',
+
+  props: {
+    text: {
+      type: String,
+      required: true
+    }
+  },
+
+  setup(props) {
+    return {
+      props
+    }
+  }
+})
+</script>
+
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,16 +1,74 @@
 <template>
   <div class="my-10">
     <v-row justify="center" align="center">
-      <v-col cols="12" md="6">
+      <v-col class="text-center" cols="12">
         <h2 class="text-xl">
           <vue-typer :text="typingTextList" />
         </h2>
         <h1 class="mb-5 text-6xl">
           増田 健太郎
         </h1>
-        <p class="mb-5">
-          With a clear vision to create and maintain the most sustainable and strong meme economy on the Binance Smart Chain (BSC), both the contract and project was created carefully by the team.
-        </p>
+        <div class="px-15">
+          <HashTag
+            v-for="(tag, key) in hashTagList"
+            :key="key"
+            :text="tag"
+            class="mx-1"
+          />
+        </div>
+      </v-col>
+      <v-col class="text-center" cols="12">
+        <h2 class="text-xl">
+          About me
+        </h2>
+      </v-col>
+      <v-col cols="12" md="4">
+        <v-card>
+          <v-card-title>
+            経歴
+          </v-card-title>
+          <v-card-text>
+            <v-btn
+              block
+              depressed
+              color="indigo"
+            >
+              read more
+            </v-btn>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="4">
+        <v-card>
+          <v-card-title>
+            職歴
+          </v-card-title>
+          <v-card-text>
+            <v-btn
+              block
+              depressed
+              color="indigo"
+            >
+              read more
+            </v-btn>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="4">
+        <v-card>
+          <v-card-title>
+            目標
+          </v-card-title>
+          <v-card-text>
+            <v-btn
+              block
+              depressed
+              color="indigo"
+            >
+              read more
+            </v-btn>
+          </v-card-text>
+        </v-card>
       </v-col>
     </v-row>
   </div>
@@ -22,6 +80,10 @@ import { defineComponent, ref } from '@nuxtjs/composition-api'
 export default defineComponent({
   name: 'IndexPage',
 
+  components: {
+    HashTag: () => import('~/components/atoms/HashTag.vue')
+  },
+
   setup() {
     const typingTextList = ref([
       'My portfolio.',
@@ -29,9 +91,23 @@ export default defineComponent({
       'I love development.',
       ':)',
     ])
+    const hashTagList = ref([
+      'AWS',
+      'Vue.js',
+      'Solidity',
+      'PHP',
+      'Node.js',
+      'Python',
+      'SQL',
+      'Nuxt.js',
+      'Laravel',
+      'Express.js',
+      'DJango',
+    ])
 
     return {
-      typingTextList
+      typingTextList,
+      hashTagList
     }
   },
 })
