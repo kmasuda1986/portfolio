aeb3536afd17ee55722ab8bee81b44a22d0efcda
kmasuda1986 <kentarou_masuda@hacklab-tech.io>
2022-05-03 15:19:38 +0900

    feat: AWS amplifyを追加

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,49 +1,23 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="data.drawer"
-      :mini-variant="data.miniVariant"
-      :clipped="data.clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in data.items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar :clipped-left="data.clipped" fixed app>
-      <v-app-bar-nav-icon @click.stop="data.drawer = !data.drawer" />
-    </v-app-bar>
     <v-main>
       <v-container>
         <Nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer v-model="data.rightDrawer" :right="data.right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light> mdi-repeat </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :absolute="!data.fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+    <v-footer
+      dark
+      padless
+    >
+      <v-card
+        class="flex text-center"
+        flat
+        tile
+      >
+        <v-card-text class="white--text">
+          © {{ new Date().getFullYear() }} Kentaro Masuda.
+        </v-card-text>
+      </v-card>
     </v-footer>
   </v-app>
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,72 +1,126 @@
 <template>
-  <div class="my-10">
-    <v-row justify="center" align="center">
+  <div class="py-15">
+    <v-row class="header-box" justify="center" align="center">
       <v-col class="text-center" cols="12">
-        <h2 class="text-xl">
-          <vue-typer :text="typingTextList" />
-        </h2>
-        <h1 class="mb-5 text-6xl">
-          増田 健太郎
+        <h1>
+          Port<span class="light-blue--text">folio</span>
         </h1>
-        <div class="px-15">
-          <HashTag
-            v-for="(tag, key) in hashTagList"
-            :key="key"
-            :text="tag"
-            class="mx-1"
-          />
+        <div class="mb-8">
+          <vue-typer :text="typingTextList" />
         </div>
       </v-col>
+    </v-row>
+
+    <v-row class="mt-10 mb-15" justify="center" align="center">
       <v-col class="text-center" cols="12">
-        <h2 class="text-xl">
-          About me
-        </h2>
+        <h1>概要</h1>
+      </v-col>
+      <v-col class="text-center" cols="12">
+        <p>
+          増田健太郎のポートフォリオ情報を掲載したサイトです<v-icon class="pb-1">mdi-account</v-icon>
+        </p>
+      </v-col>
+    </v-row>
+
+    <v-row class="mt-10 mb-15" justify="center" align="center">
+      <v-col class="text-center" cols="12">
+        <h1>スキル</h1>
       </v-col>
       <v-col cols="12" md="4">
-        <v-card>
-          <v-card-title>
-            経歴
-          </v-card-title>
+        <v-card
+          class="py-2 text-center"
+          outlined
+        >
           <v-card-text>
-            <v-btn
-              block
-              depressed
-              color="indigo"
-            >
-              read more
-            </v-btn>
+            <h1 class="mb-3 text-2xl">Webアプリ開発</h1>
+            <p>
+              サーバー構築、フロントエンド、バックエンドの幅広い開発を行うことができます。
+            </p>
+            <div>
+              <v-chip label small outlined color="light-blue">AWS</v-chip>
+              <v-chip label small outlined color="light-blue">Vue.js</v-chip>
+              <v-chip label small outlined color="light-blue">PHP</v-chip>
+              <v-chip label small outlined color="light-blue">Python</v-chip>
+              <v-chip label small outlined color="light-blue">Node.js</v-chip>
+            </div>
           </v-card-text>
         </v-card>
       </v-col>
       <v-col cols="12" md="4">
-        <v-card>
-          <v-card-title>
-            職歴
-          </v-card-title>
+        <v-card
+          class="pb-2 text-center"
+          outlined
+        >
           <v-card-text>
-            <v-btn
-              block
-              depressed
-              color="indigo"
-            >
-              read more
-            </v-btn>
+            <h2 class="my-3">ブロックチェーン開発</h2>
+            <p>
+              Binance Smart Chainを使用してERC20トークンを開発することができます。
+            </p>
+            <div>
+              <v-chip label small outlined color="light-blue">BSC</v-chip>
+              <v-chip label small outlined color="light-blue">Solidity</v-chip>
+              <v-chip label small outlined color="light-blue">Hardhat</v-chip>
+              <v-chip label small outlined color="light-blue">Metamask</v-chip>
+            </div>
           </v-card-text>
         </v-card>
       </v-col>
       <v-col cols="12" md="4">
-        <v-card>
-          <v-card-title>
-            目標
-          </v-card-title>
+        <v-card
+          class="pb-2 text-center"
+          outlined
+        >
           <v-card-text>
-            <v-btn
-              block
-              depressed
-              color="indigo"
-            >
-              read more
-            </v-btn>
+            <h2 class="my-3">人材育成</h2>
+            <p>
+              プログラミングスクールの運営をしていた経験があり、講師をすることができます。
+            </p>
+            <div>
+              <v-chip label small outlined color="light-blue">AWS</v-chip>
+              <v-chip label small outlined color="light-blue">Docker</v-chip>
+              <v-chip label small outlined color="light-blue">Nuxt.js</v-chip>
+              <v-chip label small outlined color="light-blue">Laravel</v-chip>
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-row class="mt-10 mb-15" justify="center" align="center">
+      <v-col class="text-center" cols="12">
+        <h1>職歴</h1>
+      </v-col>
+      <v-col
+        v-for="(career, key) in careerList"
+        :key="key"
+        cols="12"
+        md="4"
+      >
+        <v-card outlined height="250px">
+          <v-card-title class="pb-0">
+            <span class="mr-2 light-blue--text">No.{{ career.no }}</span>
+          </v-card-title>
+          <v-card-title class="pt-0">
+            {{ career.name }}
+          </v-card-title>
+          <v-card-subtitle>
+            <span>
+              {{ `${career.startAt.getFullYear()}年${career.startAt.getMonth()}月` }}
+            </span>
+             -
+            <span v-if="career.endAt !== null">
+            {{ `${career.endAt.getFullYear()}年${career.endAt.getMonth()}月` }}
+            </span>
+            <span v-else class="cyan--text">
+              現在
+            </span>
+            <span>
+              （{{ career.position }}）
+            </span>
+          </v-card-subtitle>
+          <v-card-text>
+            <!-- eslint-disable-next-line vue/no-v-html -->
+            <p v-html="career.descriptioin" />
           </v-card-text>
         </v-card>
       </v-col>
@@ -74,22 +128,17 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import { defineComponent, ref } from '@nuxtjs/composition-api'
 
 export default defineComponent({
   name: 'IndexPage',
-
-  components: {
-    HashTag: () => import('~/components/atoms/HashTag.vue')
-  },
 
   setup() {
     const typingTextList = ref([
       'My portfolio.',
       'Welcome to my site.',
       'I love development.',
-      ':)',
     ])
     const hashTagList = ref([
       'AWS',
@@ -104,14 +153,137 @@ export default defineComponent({
       'Express.js',
       'DJango',
     ])
+    const careerList = ref([
+      {
+        no: 8,
+        name: '株式会社RuckPlus',
+        startAt: new Date(2022, 1, 1),
+        endAt: null,
+        position: 'CEO',
+        descriptioin: '過去の営業経験を活かしてSES事業に特化した法人を設立。役員2名・営業2名・アルバイト1名の全5名で構成。<br>10名以上のフリーランスエンジニアが稼働中。'
+      },
+      {
+        no: 7,
+        name: 'HackLab株式会社',
+        startAt: new Date(2020, 7, 1),
+        endAt: null,
+        position: 'CEO',
+        descriptioin: '世の中のサービスをdAppsで再構築することを目的とした法人を設立。<br>現在、資金を集めるためにWebアプリの受託開発を行う。'
+      },
+      {
+        no: 6,
+        name: '個人事業主',
+        startAt: new Date(2020, 1, 1),
+        endAt: new Date(2020, 6, 0 + 1),
+        position: 'エンジニア',
+        descriptioin: 'フリーランスエンジニアとして開業し、主にAWS/PHPの技術を中心とした案件に従事。<br>Ethereumなどのブロックチェーン技術の学習を行う。'
+      },
+      {
+        no: 5,
+        name: 'シアトルコンサルティング株式会社',
+        startAt: new Date(2019, 3, 1),
+        endAt: new Date(2019, 12, 0 + 1),
+        position: 'エンジニア',
+        descriptioin: '人生初転職。ブロックチェーン開発事業部 チーフエンジニアとして従事。様々なブロックチェーンの開発を経験。'
+      },
+      {
+        no: 4,
+        name: '株式会社EPARK',
+        startAt: new Date(2018, 4, 1),
+        endAt: new Date(2019, 2, 0 + 1),
+        position: 'PM/SE',
+        descriptioin: '2500万会員の認証基盤プラットフォームのプロジェクトマネージャーに抜擢。<br>AWSやPHPの知識を身につけ、要件定義から実装、保守運用まで幅広い業務に携わる。'
+      },
+      {
+        no: 3,
+        name: '株式会社EPARK',
+        startAt: new Date(2016, 11, 1),
+        endAt: new Date(2018, 3, 0 + 1),
+        position: 'PL/SE',
+        descriptioin: 'Web決済プラットフォームのプロジェクトリーダーに抜擢。<br>クレジットカードの業務フローや会計業務の知識を身につける。'
+      },
+      {
+        no: 2,
+        name: '株式会社光通信',
+        startAt: new Date(2015, 4, 1),
+        endAt: new Date(2016, 10, 0 + 1),
+        position: 'マネージャー',
+        descriptioin: '名古屋エリアの新規立ち上げを目標に、50名規模の責任者として抜擢。<br>閉店予定だった店舗を名古屋で月間新規販売台数ベスト３の店舗までに引き上げる。'
+      },
+      {
+        no: 1,
+        name: '株式会社光通信',
+        startAt: new Date(2009, 4, 1),
+        endAt: new Date(2015, 3, 0 + 1),
+        position: '一般社員',
+        descriptioin: '新卒で入社しソフトバンクショップの販売スタッフとして従事。<br>新卒１年目で関西で販売台数No1のソフトバンク心斎橋 副店長に抜擢。'
+      }
+    ])
 
     return {
       typingTextList,
-      hashTagList
+      hashTagList,
+      careerList,
     }
   },
 })
 </script>
 
 <style scoped>
+@media screen and (max-width: 600px) {
+  .header-box {
+    margin-top: 230px;
+    margin-bottom: 230px;
+  }
+
+  .header-box h1 {
+    font-size: 50px;
+  }
+
+  .header-box p {
+    font-size: 16px;
+  }
+
+  .header-box .find-btn {
+    margin-bottom: 200px;
+  }
+}
+
+@media screen and (min-width: 600px) and (max-width: 960px) {
+  .header-box {
+    margin-top: 260px;
+    margin-bottom: 200px;
+  }
+
+  .header-box h1 {
+    font-size: 80px;
+  }
+
+  .header-box p {
+    font-size: 20px;
+  }
+
+  .header-box .find-btn {
+    margin-bottom: 200px;
+  }
+}
+
+@media screen and (min-width: 960px) {
+  .header-box {
+    margin-top: 260px;
+    margin-bottom: 350px;
+  }
+
+  .header-box h1 {
+    font-size: 120px;
+  }
+
+  .header-box p {
+    font-size: 20px;
+  }
+
+  .header-box .find-btn {
+    margin-bottom: 200px;
+  }
+}
 </style>
