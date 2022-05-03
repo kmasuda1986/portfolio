bf4a913fc23fe5f2a970fb0ae149a6f694c25940
kmasuda1986 <kentarou_masuda@hacklab-tech.io>
2022-05-03 23:12:03 +0900

    fix: debug

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,7 +3,7 @@
     <v-row class="header-box" justify="center" align="center">
       <v-col class="text-center" cols="12">
         <h1>
-          Port<span class="light-blue--text">folio</span>
+          Port<span class="light-blue--text">folio</span>{{ $config.googleAnalytics }}
         </h1>
         <div class="mb-8">
           <vue-typer :text="typingTextList" />
