d5c11d5896abfb109d6d3795fdfae98a2db44a15
kmasuda1986 <kentarou_masuda@hacklab-tech.io>
2022-05-08 20:19:41 +0900

    fix: 修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,7 +43,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '@/plugins/amplify.ts', mode: 'client' },
+    { src: '~/plugins/amplify.ts', mode: 'client' },
     { src: '~/plugins/vue-typer.ts', mode: 'client' },
   ],
 
